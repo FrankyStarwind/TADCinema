94c8d46d111b2c5741daeee43bfa7eb65d15d422
carlosmz96 <carlosmz96esp@gmail.com>
2021-04-29 20:11:22 +0200

    Datos del usuario
    Poder comprobar y validar campos, además de modificar sus datos. Incluido un componente window a modo de popup.

--- src/main/java/com/mycompany/interfaces/PerfilUI.java
+++ src/main/java/com/mycompany/interfaces/PerfilUI.java
@@ -1,5 +1,11 @@
 package com.mycompany.interfaces;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
 import com.vaadin.annotations.PreserveOnRefresh;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
@@ -11,76 +17,303 @@ import com.vaadin.ui.Button;
 import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Notification;
 import com.vaadin.ui.Panel;
 import com.vaadin.ui.PasswordField;
+import com.vaadin.ui.PopupView;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Window;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.annotation.WebServlet;
 
 @Theme("mytheme")
 @PreserveOnRefresh
 public class PerfilUI extends UI {
 
+    public static boolean cambiaClave = false;
+    public static String dniUsuario = "";
+
     @Override
     protected void init(VaadinRequest request) {
         final WrappedSession session = getSession().getSession();
         final VerticalLayout rootLayout = new VerticalLayout();
+        final HorizontalLayout botonera = new HorizontalLayout();
+        final VerticalLayout vInterior = new VerticalLayout();
         final Button btnLogout = new Button("Cerrar sesión");
-        
+
+        // comprueba si la sesión está iniciada
         comprobarSesion(rootLayout, session);
-        
+
         // invalida la sesion y redirecciona a login
         btnLogout.addClickListener(e -> {
             session.invalidate();
             Page.getCurrent().setLocation("/");
         });
-        
-        final Panel userPanel = cargarMenu();
+
+        // acceso a la base de datos
+        MongoClient mongoClient = null;
+        try {
+            mongoClient = new MongoClient("localhost", 27017);
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(PerfilUI.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        final DB db = mongoClient.getDB("TADCinemaDB");
+        // tabla usuarios
+        final DBCollection usuarios = db.getCollection("usuarios");
+
+        // panel para la navegación
+        final Panel navPanel = cargarMenu();
+        // panel para el formulario de datos de usuario
         final Panel datosPanel = new Panel("Mis datos");
-        
+
+        final List<TextField> listaCampos = new ArrayList<>();
+
+        // formulario de datos de usuario
         final FormLayout formDatos = new FormLayout();
         final TextField nombre = new TextField("Nombre");
+        listaCampos.add(nombre);
         final TextField apellidos = new TextField("Apellidos");
+        listaCampos.add(apellidos);
         final TextField dni = new TextField("Dni");
-        final TextField telefono = new TextField("Telefono");
+        listaCampos.add(dni);
+        final TextField telefono = new TextField("Teléfono");
+        listaCampos.add(telefono);
         final TextField username = new TextField("Usuario");
+        listaCampos.add(username);
         final PasswordField password = new PasswordField("Contraseña");
         final PasswordField newPassword = new PasswordField("Nueva contraseña");
         final Button btnGuardar = new Button("Guardar datos");
-        formDatos.addComponents(nombre, apellidos, dni, telefono, username, password, newPassword, btnGuardar);
+        btnGuardar.setStyleName("primary");
+        final Button btnEliminar = new Button("Eliminar cuenta");
+        btnEliminar.setStyleName("danger");
+
+        // carga los datos del usuario
+        cargarDatosCampos(listaCampos, session, usuarios);
+
+        // clic al botón de guardar datos
+        btnGuardar.addClickListener(e -> {
+            if (validarCampos(listaCampos, password, newPassword, usuarios)) {
+                // Edición de los datos de usuario
+                BasicDBObject actualizarUsuario = new BasicDBObject();
+                // Objeto usuario
+                BasicDBObject usuarioEdit = new BasicDBObject();
+                usuarioEdit.append("nombre", nombre.getValue());
+                usuarioEdit.append("apellidos", apellidos.getValue());
+                usuarioEdit.append("_id", dni.getValue());
+                usuarioEdit.append("telefono", telefono.getValue());
+                usuarioEdit.append("username", username.getValue());
+                if (cambiaClave) {
+                    usuarioEdit.append("contraseña", newPassword.getValue());
+                }
+                // set usuario a actualizar
+                actualizarUsuario.put("$set", usuarioEdit);
+                // buscar por id
+                BasicDBObject buscarPorId = new BasicDBObject();
+                buscarPorId.append("_id", dni.getValue());
+                // actualiza el elemento por id
+                usuarios.update(buscarPorId, actualizarUsuario);
+                Notification.show("Los datos se han guardado correctamente.", Notification.Type.TRAY_NOTIFICATION);
+                // limpiar campos de contraseña
+                password.setValue("");
+                newPassword.setValue("");
+            }
+        });
+        
+        final HorizontalLayout botoneraPopup = new HorizontalLayout();
+        final Button btnConfirmar = new Button("Eliminar");
+        btnConfirmar.setStyleName("danger");
+        final Button btnCancelar = new Button("Cancelar");
+        botoneraPopup.addComponents(btnConfirmar, btnCancelar);
+        botoneraPopup.setMargin(true);
+        botoneraPopup.setSpacing(true);
+        final Window ventanaConfirmacion = new Window("¿Estás seguro?");
+        ventanaConfirmacion.center();
+        ventanaConfirmacion.setClosable(false);
+        ventanaConfirmacion.setDraggable(false);
+        ventanaConfirmacion.setResizable(false);
+        ventanaConfirmacion.setContent(botoneraPopup);
+        
+        
+        // clic al botón eliminar
+        btnEliminar.addClickListener(e -> {
+            addWindow(ventanaConfirmacion);
+        });
+        
+        // FALTA ELIMINAR CUENTA
+
+        // ESTRUCTURA DE COMPONENTES
+        botonera.addComponents(btnGuardar, btnEliminar);
+        botonera.setSpacing(true);
+
+        formDatos.addComponents(nombre, apellidos, dni, telefono, username, password, newPassword, botonera);
         formDatos.setMargin(true);
-        formDatos.setSpacing(true);
-        datosPanel.setContent(formDatos);
-        
-        rootLayout.addComponents(btnLogout, userPanel, datosPanel);
-        
+
+        vInterior.setMargin(true);
+        vInterior.addComponents(new Label("A continuación, puedes modificar tus datos en este formulario:"), formDatos);
+        datosPanel.setContent(vInterior);
+
+        rootLayout.addComponents(btnLogout, navPanel, datosPanel);
+
         rootLayout.setMargin(true);
         rootLayout.setSpacing(true);
-        
+
         setContent(rootLayout);
     }
-    
+
     @WebServlet(urlPatterns = "/perfil/*", name = "PerfilUIServlet", asyncSupported = true)
     @VaadinServletConfiguration(ui = PerfilUI.class, productionMode = false)
     public static class PerfilUIServlet extends VaadinServlet {
     }
-    
+
     /**
-     * Método encargado de comprobar si la sesión existe o no
-     * Si no existe, redirecciona al login
+     * Método encargado de comprobar si la sesión existe o no Si no existe,
+     * redirecciona al login
      */
     private static void comprobarSesion(final VerticalLayout rootLayout, final WrappedSession session) {
-        if(session.getAttribute("usuario") == null){
+        if (session.getAttribute("usuario") == null) {
             Page.getCurrent().setLocation("/");
         } else {
             final Label bienvenido = new Label("Bienvenido, " + session.getAttribute("usuario"));
             rootLayout.addComponent(bienvenido);
         }
     }
-    
+
+    /**
+     * Método encargado de settear los datos del usuario
+     *
+     * @param campos listado de campos a settear
+     * @param session sesión activa
+     */
+    private static void cargarDatosCampos(final List<TextField> campos, final WrappedSession session,
+            final DBCollection usuarios) {
+        final DBCursor cursor = usuarios.find();
+
+        DBObject itUsuario = null;
+        DBObject usuario = null;
+        while (cursor.hasNext()) {
+            itUsuario = cursor.next();
+            if (itUsuario.get("username").equals(session.getAttribute("usuario"))) {
+                usuario = itUsuario;
+                break;
+            }
+        }
+
+        if (usuario != null) {
+            for (TextField campo : campos) {
+                switch (campo.getCaption()) {
+                    case "Nombre":
+                        campo.setValue(usuario.get("nombre").toString());
+                        break;
+                    case "Apellidos":
+                        campo.setValue(usuario.get("apellidos").toString());
+                        break;
+                    case "Dni":
+                        campo.setValue(usuario.get("_id").toString());
+                        dniUsuario = usuario.get("_id").toString();
+                        break;
+                    case "Teléfono":
+                        campo.setValue(usuario.get("telefono").toString());
+                        break;
+                    case "Usuario":
+                        campo.setValue(usuario.get("username").toString());
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
+    /**
+     * Método encargado de comprobar si los campos del formulario de datos del
+     * usuario son válidos o no
+     *
+     * @param campos listado de campos de texto
+     * @param pass campo de contraseña
+     * @param newPass campo de nueva nueva contraseña
+     * @return TRUE/FALSE
+     */
+    private static boolean validarCampos(final List<TextField> campos, final PasswordField pass,
+            final PasswordField newPass, final DBCollection usuarios) {
+        final DBCursor cursor = usuarios.find();
+        boolean sonValidos = true;
+        String clave = "";
+        
+        DBObject usuario = null;
+        while(cursor.hasNext()) {
+            usuario = cursor.next();
+            if(usuario.get("_id").equals(dniUsuario)) {
+                clave = usuario.get("contraseña").toString();
+            }
+        }
+
+        String errores = "";
+
+        for (TextField campo : campos) {
+            switch (campo.getCaption()) {
+                case "Nombre":
+                    if (campo.getValue().equals("")) {
+                        errores += "El campo 'Usuario' no puede estar vacío";
+                        sonValidos = false;
+                    }
+                    break;
+                case "Apellidos":
+                    if (campo.getValue().equals("")) {
+                        errores += "El campo 'Apellidos' no puede estar vacío";
+                        sonValidos = false;
+                    }
+                    break;
+                case "Dni":
+                    if (campo.getValue().equals("")) {
+                        errores += "El campo 'Dni' no puede estar vacío";
+                        sonValidos = false;
+                    }
+                    break;
+                case "Usuario":
+                    if (campo.getValue().equals("")) {
+                        errores += "El campo 'Usuario' no puede estar vacío";
+                        sonValidos = false;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        cambiaClave = false;
+        if (!pass.getValue().equals("") && newPass.getValue().equals("")) {
+            errores += "Debes de escribir una nueva contraseña";
+            sonValidos = false;
+        } else if (pass.getValue().equals("") && !newPass.getValue().equals("")) {
+            errores += "Debes de escribir tu contraseña actual";
+            sonValidos = false;
+        } else if (!pass.getValue().equals("") && !newPass.getValue().equals("")) {
+            if (clave.equals(pass.getValue())) {
+                cambiaClave = true;
+            } else {
+                errores += "La contraseña es incorrecta.";
+                sonValidos = false;
+            }
+        } else {
+            errores += "\n";
+        }
+
+        if (!sonValidos) {
+            Notification.show("Error", errores, Notification.Type.ERROR_MESSAGE);
+        }
+
+        return sonValidos;
+    }
+
     /**
      * Método encargado de cargar el menú de navegación
+     *
      * @return Panel
      */
     private static Panel cargarMenu() {
@@ -89,25 +322,25 @@ public class PerfilUI extends UI {
         final Button btnInicio = new Button("Inicio");
         final Button btnCartelera = new Button("Cartelera");
         final Button btnPerfil = new Button("Perfil");
-        
+
         btnInicio.addClickListener(e -> {
             Page.getCurrent().setLocation("/home");
         });
-        
+
         btnCartelera.addClickListener(e -> {
             Page.getCurrent().setLocation("/cartelera");
         });
-        
+
         btnPerfil.addClickListener(e -> {
             Page.getCurrent().setLocation("/perfil");
         });
-        
+
         hLayout.addComponents(btnInicio, btnCartelera, btnPerfil);
         hLayout.setMargin(true);
         hLayout.setSpacing(true);
         userPanel.setContent(hLayout);
-        
+
         return userPanel;
     }
-    
+
 }
